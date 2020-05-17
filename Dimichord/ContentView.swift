bb537551f80fa608e952a84d060f2c62a355262d
Vinicius Mello <vinicius.moreira.mello@gmail.com>
2020-05-17 11:28:26 -0300

    Initial Commit

---
+++ Dimichord/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dimichord
+//
+//  Created by Vinicius Moreira on 17/05/20.
+//  Copyright © 2020 Vinicius Mello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DimichordTests/DimichordTests.swift
@@ -0,0 +1,34 @@
+//
+//  DimichordTests.swift
+//  DimichordTests
+//
+//  Created by Vinicius Moreira on 17/05/20.
+//  Copyright © 2020 Vinicius Mello. All rights reserved.
+//
+
+import XCTest
+@testable import Dimichord
+
+class DimichordTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
